8c2f7fbdf824ae400d573def891dbabaeef3ca68
Piyush11204 <138059020+Piyush11204@users.noreply.github.com>
2024-12-05 20:49:39 +0530

    fix for development

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+
 import { Outlet } from "react-router-dom";
 // import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer.jsx";
 import { Toaster } from "react-hot-toast";
 import { UserContextProvider } from "../context/userContext.jsx";
-import Navbar from "./components/Navbar/Navbar";
-import ScrollToTop from "./Pages/Home/ScrollToTop";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
 
 function Layout() {
   return (
